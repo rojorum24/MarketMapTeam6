2e167a62264a5427512f1610db95af99833ab7a8
jwingart-pg <116602102+jwingart-pg@users.noreply.github.com>
2024-05-26 16:31:52 -0700

    Updated App icon, splash page graphic, nav bar color scheme

--- MarketMapTeam6.Android/MainActivity.cs
+++ MarketMapTeam6.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace MarketMapTeam6.Droid
 {
-    [Activity(Label = "MarketMapTeam6", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "MarketMapTeam6", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
