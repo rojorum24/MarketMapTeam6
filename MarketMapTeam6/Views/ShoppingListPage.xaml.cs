5e69d0f2325fccf3538930ff528a342c40da5274
rojorum24 <rojorum711@proton.me>
2024-05-28 17:13:00 -0400

    Added CollectionView to the shopping List page with basic placeholder list

--- MarketMapTeam6/Views/ShoppingListPage.xaml.cs
+++ MarketMapTeam6/Views/ShoppingListPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,18 @@ namespace MarketMapTeam6.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ShoppingListPage : ContentPage
     {
+        private readonly string[] storeItems = new[] { "Carrots", "Peas", "Onions", "Ginger", "Peppers",
+        "Brocolli"};
+
+        public ObservableCollection<string> StoreItems { get; set; }
         public ShoppingListPage()
         {
             InitializeComponent();
-            List<String> testList = new List<String>
-            {
-                "Item1",
-                "Item2",
-                "Item3"
-            };
-            //ShoppingListPage.ItemsSource
+
+            StoreItems = new ObservableCollection<string>(storeItems);
+
+            BindingContext = this;
         }
+
     }
 }
